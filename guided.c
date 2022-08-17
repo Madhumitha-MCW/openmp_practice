f9b0cb395dedb4e571818a76b97cb34ed8257511
Madhumitha-MCW <109727353+Madhumitha-MCW@users.noreply.github.com>
2022-08-18 00:48:03 +0530

    Add files via upload

---
+++ guided.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<omp.h>
+#include <time.h>
+
+int main()
+{
+    clock_t t;
+    t = clock();
+    #pragma omp parallel for schedule(guided, 3)	
+    for (int i = 0; i < 20; i++)
+    {
+        printf("Thread %d is running number %d\n", omp_get_thread_num(), i);
+    }
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds   
+    printf("\nloop took %f seconds to execute \n", time_taken);
+    return 0;
+}
